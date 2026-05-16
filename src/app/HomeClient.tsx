2f6bff278c5a9299ebec99a21ca3ecaf9f7100d9
Muhammad Ali Raza <muhammadaliraza7530@gmail.com>
2026-05-16 17:22:59 +0500

    Update HomeClient.tsx

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { motion } from "motion/react";
-import React from "react";
-import { Project, Testimonial } from "@/types";
-import Hero from "../components/home/Hero";
-import FeaturedProjects from "@/components/home/FeaturedProjects";
-import Testimonials from "@/components/home/Testimonials";
+import { motion } from 'framer-motion';
+import React from 'react';
+import { Project, Testimonial } from '@/types';
+import Hero from '../components/home/Hero';
+import FeaturedProjects from '@/components/home/FeaturedProjects';
+import Testimonials from '@/components/home/Testimonials';
 
 interface HomeClientProps {
   initialProjects: Project[];
@@ -14,11 +14,11 @@ interface HomeClientProps {
 
 export default function HomeClient({ initialProjects, initialTestimonials }: HomeClientProps) {
   return (
-    <div className="flex flex-col bg-bg">
+    <div className='flex flex-col bg-bg'>
       <Hero />
-      
-      <section className="px-6 lg:px-10 py-32 bg-[#080808] border-border">
-        <div className="max-w-[1290px] mx-auto space-y-40">
+
+      <section className='px-6 lg:px-10 py-32 bg-[#080808] border-border'>
+        <div className='max-w-[1290px] mx-auto space-y-40'>
           <FeaturedProjects projects={initialProjects} />
           <Testimonials testimonials={initialTestimonials} />
         </div>
